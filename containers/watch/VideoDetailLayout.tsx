410a6b22652a124728d49e6a1b4c4da781f9d4c2
ys <hootingowl09@pusan.ac.kr>
2023-09-26 10:25:59 +0900

    Feat: 영상세부조회 페이지 API 연동
    - SSR로 작성했으나, 추후 SSG로 리팩토링 필요
    - 유저 프로필 기본 이미지 설정

--- containers/watch/VideoDetailLayout.tsx
+++ containers/watch/VideoDetailLayout.tsx
@@ -1,5 +1,6 @@
 import CommentInput from '@/components/comment/CommentInput';
 import CommentItem from '@/components/comment/CommentItem';
+import Avatar from '@/components/ui/Avatar';
 import VideoSummaryItemRow from '@/components/video/VideoSummaryItemRow';
 import { VideoDetailType } from '@/types/videoType';
 
@@ -16,14 +17,29 @@ export default function VideoDetailLayout({ video }: VideoDetailLayoutProps) {
             비디오
           </div>
         </div>
-        <div className='rounded-md p-4 w-full mx-auto my-4 border'>비디오 정보</div>
+        <div className='rounded-md p-4 w-full mx-auto my-4 border'>
+          <div className='py-5'>좋아요버튼이 들어갈곳.. </div>
+          <div className='flex items-center'>
+            <div>
+              <Avatar width={12} imgUrl={video.channelProfileUrl} marginX={3} />
+            </div>
+            <div>{video.channelName || '채널명'}</div>
+          </div>
+          <div className='mx-3 py-5'>{video.content}</div>
+        </div>
         <div className='w-full mx-1 my-5 max-2xl:w-full'>
           <CommentInput />
-          <div className='w-full'>{Array(8).fill(<CommentItem />)}</div>
+          <div className='w-full'>
+            {Array(8)
+              .fill(null)
+              .map((_, index) => (
+                <CommentItem key={index} />
+              ))}
+          </div>
         </div>
       </div>
       <div className='basis-1/4 max-2xl:hidden my-4 shrink-0'>
-        {video.recommendedVideoList?.map((v) => <VideoSummaryItemRow key={v.videoId} videoSummaryItem={v} />)}
+        {video.recommendedList?.map((v) => <VideoSummaryItemRow key={v.videoId} videoSummaryItem={v} />)}
       </div>
     </div>
   );
