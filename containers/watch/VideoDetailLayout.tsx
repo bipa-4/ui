019de57283634b3747b78cc9cd6f75c4d9f8837b
ys <hootingowl09@pusan.ac.kr>
2023-10-10 15:41:05 +0900

    Style: 포매팅

--- containers/watch/VideoDetailLayout.tsx
+++ containers/watch/VideoDetailLayout.tsx
@@ -39,7 +39,6 @@ export default function VideoDetailLayout({ video }: VideoDetailLayoutProps) {
       setLike(!like);
       const res = await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}/video/detail/${video.videoId}/like`);
       console.log(res);
-      
     }
   };
 
