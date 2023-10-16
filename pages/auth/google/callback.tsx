6d1610de6ae25f4393927f0af4f016672ccc236d
ys <hootingowl09@pusan.ac.kr>
2023-10-16 11:15:09 +0900

    Fix: 로그인 로직 수정
    - 훅 사용하지 않고 초기렌더링 시 인증, 인가 가능하도록 수정

--- pages/auth/google/callback.tsx
+++ pages/auth/google/callback.tsx
@@ -1,40 +1,33 @@
 import { userAtom } from '@/components/layouts/Header';
-import useMemberData from '@/hooks/useMemberData';
-import axios from 'axios';
-import { useSetAtom } from 'jotai';
+import fetcher from '@/utils/axiosFetcher';
+import { useAtom } from 'jotai';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 
 export default function Callback() {
   const router = useRouter();
-  const setUser = useSetAtom(userAtom);
+  const [user, setUser] = useAtom(userAtom);
   const authCode = router.asPath;
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
   useEffect(() => {
     const auth = async () => {
-      const res = await axios.get(BASE_URL + authCode, { withCredentials: true });
-      console.log('콜백 페이지 응답 !', res);
+      try {
+        const authRes = await fetcher(BASE_URL + authCode);
+        const userRes = await fetcher(`${BASE_URL}/account/check`);
+        setUser(userRes.data);
+        console.log('authRes !', authRes);
+        console.log('user !', user);
+      } catch (error) {
+        console.log('error !', error);
+      }
     };
     auth();
   }, []);
 
-  const { userInfo, error } = useMemberData();
+  //const { userInfo, error } = useMemberData();
 
-  useEffect(() => {
-    if (error) {
-      console.log('유저 정보 불러오기 실패', error);
-    }
-
-    if (!userInfo) {
-      setUser(null);
-    }
-
-    setUser(userInfo);
-    router.push('/');
-  }, [userInfo]);
-
-  if (userInfo) {
+  if (user) {
     router.push('/');
   }
 
