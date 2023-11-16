4aeaaecca253887e94afa2ccfe44d7a7fd9ac177
ys <hootingowl09@pusan.ac.kr>
2023-11-16 14:48:25 +0900

    Fix: 카테고리별 조회 무한스크롤 로직 수정
    - 로딩스피너가 계속 표시되는 문제 수정
    - meta tag 추가

--- pages/category/[catId].tsx
+++ pages/category/[catId].tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
 import InfiniteVideoContainer from '@/components/video/InfiniteVideoContainer';
 import useCategoryList from '@/hooks/useCategoryList';
@@ -22,33 +23,24 @@ export default function Category({ catId, categoryVideos }: CategoryProps) {
   const [nextId, setNextId] = useState('');
   const [videoList, setVideoList] = useState<VideoCardType[]>([]);
   const { categoryList } = useCategoryList();
+  const categoryName = categoryList?.find((category) => category.categoryNameId === catId)?.categoryName;
   const router = useRouter();
 
   useEffect(() => {
     setVideoList(categoryVideos.videos);
     setNextId(categoryVideos.nextUUID);
-    setHasMore(true);
-    console.log(categoryVideos);
-  }, [categoryVideos.videos]);
-
-  useEffect(() => {
-    if (categoryVideos.nextUUID === null) {
-      setHasMore(false);
-    }
-  }, []);
+    categoryVideos.nextUUID === null ? setHasMore(false) : setHasMore(true);
+  }, [catId]);
 
   const fetchVideo = async (nextUUID: string) => {
     const res = await axios.get(`${BASE_URL}/video/category/${catId}?page=${nextUUID}&pageSize=${PAGE_SIZE}`, {
       withCredentials: true,
     });
-    console.log('fetchVideo', res.data);
     setNextId(res.data.nextUUID);
     return res.data;
   };
 
   const fetchMoreData = async () => {
-    console.log('fetchMoreData', nextId);
-
     if (nextId === null) {
       setHasMore(false);
       return;
@@ -67,27 +59,36 @@ export default function Category({ catId, categoryVideos }: CategoryProps) {
   }
 
   return (
-    <div className='px-32 min-h-screen bg-base-100 max-xl:px-5'>
-      <div className='tabs w-full m-3 mt-8'>
-        {categoryList?.map((category) => (
-          <div
-            className={`tab tab-lg tab-bordered w-36 ${
-              category.categoryNameId === catId && 'tab-active'
-            } cursor-pointer hover:bg-base-100`}
-            onClick={() => router.push(`/category/${category.categoryNameId}`)}
-            key={category.categoryNameId}
-          >
-            {category.categoryName}
-          </div>
-        ))}
-      </div>
+    <>
+      <Head>
+        <title> {categoryName} | StreamWave</title>
+        <meta name='description' content='동영상 공유 플랫폼 StreamWave입니다.' />
+        <meta property='og:title' content='간편한 영상 공유 플랫폼 - StreamWave' />
+        <meta property='og:description' content='동영상 공유 플랫폼 StreamWave입니다.' />
+        <meta property='og:image' content='/images/streamWave.png' />
+      </Head>
+      <div className='px-32 min-h-screen bg-base-100 max-xl:px-5'>
+        <div className='tabs w-full m-3 mt-8'>
+          {categoryList?.map((category) => (
+            <div
+              className={`tab tab-lg tab-bordered w-36 ${
+                category.categoryNameId === catId && 'tab-active'
+              } cursor-pointer hover:bg-base-100`}
+              onClick={() => router.push(`/category/${category.categoryNameId}`)}
+              key={category.categoryNameId}
+            >
+              {category.categoryName}
+            </div>
+          ))}
+        </div>
 
-      {categoryVideos.videos.length !== 0 ? (
-        <InfiniteVideoContainer videoList={videoList} dataFetcher={fetchMoreData} hasMore={hasMore} />
-      ) : (
-        <div className='flex items-center m-auto h-36 justify-center'>업로드한 영상이 없습니다.</div>
-      )}
-    </div>
+        {categoryVideos.videos.length !== 0 ? (
+          <InfiniteVideoContainer videoList={videoList} dataFetcher={fetchMoreData} hasMore={hasMore} />
+        ) : (
+          <div className='flex items-center m-auto h-36 justify-center'>업로드한 영상이 없습니다.</div>
+        )}
+      </div>
+    </>
   );
 }
 
