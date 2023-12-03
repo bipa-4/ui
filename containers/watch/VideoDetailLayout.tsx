1f164917c6453835bc57615e045d6219c83c5b67
ys <hootingowl09@pusan.ac.kr>
2023-12-04 01:41:58 +0900

    Fix: 영상 세부 조회 수정
    - 조회수 오류 수정

--- containers/watch/VideoDetailLayout.tsx
+++ containers/watch/VideoDetailLayout.tsx
@@ -1,13 +1,12 @@
 import VideoDetailInfo from '@/components/video/VideoDetailInfo';
 import VideoSummaryItemRow from '@/components/video/VideoSummaryItemRow';
-import { VideoCardType } from '@/types/videoType';
+import { VideoCardType, VideoDetailType } from '@/types/videoType';
 import VideoPlayer from '@/components/video/VideoPlayer';
 import { useEffect, useState } from 'react';
 import Comments from '@/components/comment/Comments';
 import fetcher from '@/utils/axiosFetcher';
 import axios from 'axios';
 import { customWarningToast } from '@/utils/CustomToast';
-import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import VideoDetailInfoSkeleton from '@/components/skeleton/VideoDetailInfoSkeleton';
 import UploadLayout from '../upload/UploadLayout';
@@ -24,40 +23,38 @@ const videoArgs = {
   },
 };
 
-export default function VideoDetailLayout() {
+interface VideoDetailLayoutProps {
+  video: VideoDetailType;
+}
+
+export default function VideoDetailLayout({ video }: VideoDetailLayoutProps) {
   const [updateOpen, setUpdateOpen] = useState(false);
   const [isMyVideo, setIsMyVideo] = useState<boolean>();
-  const { vid } = useRouter().query;
-  const { data, isLoading } = useSWR(`${BASE_URL}/video/detail/${vid}`, fetcher);
 
   const checkMyVideo = async () => {
     try {
-      const checkResult = await fetcher(`${BASE_URL}/video/check?videoId=${data.videoId}`);
+      const checkResult = await fetcher(`${BASE_URL}/video/check?videoId=${video.videoId}`);
       setIsMyVideo(checkResult);
     } catch (err) {
       customWarningToast(`권한 체크 에러: ${err} 관리자에게 문의하세요.`);
     }
   };
 
-  const updateViewCount = async () => {
-    await axios.put(`${process.env.NEXT_PUBLIC_BASE_URL}/video/updateViews/${vid}`, {}, { withCredentials: true });
-  };
-
   const updateRecommendList = async () => {
-    await axios.put(`${BASE_URL}/video/updateRecommend/${data.videoId}`, {}, { withCredentials: true });
+    await axios.put(`${BASE_URL}/video/updateRecommend/${video.videoId}`, {}, { withCredentials: true });
   };
 
   useEffect(() => {
     checkMyVideo();
-    updateViewCount();
     updateRecommendList();
-  }, [data]);
+  }, []);
 
-  // console.log('video', data);
-  // console.log('video.privateType', data.privateType);
-  // console.log('isMyVideo', isMyVideo);
+  console.log('video', video);
+  console.log('updateOpen', updateOpen);
+  console.log('isMyVideo', isMyVideo);
+  console.log('------------------------------------------');
 
-  if (data.privateType && isMyVideo === false) {
+  if (video.privateType && isMyVideo === false) {
     return (
       <div className='min-h-screen flex justify-center items-center m-auto bg-base-100'>
         <h1 className='text-2xl font-bold tracking-tight '>🔒 비공개 영상입니다.</h1>
@@ -69,19 +66,19 @@ export default function VideoDetailLayout() {
     setUpdateOpen(true);
   };
 
-  if (isLoading) {
-    return (
-      <div className='w-full flex min-h-[85vh]'>
-        <div className='grow my-4'>
-          <div className='m-3/5 relative overflow-hidden' style={{ paddingTop: '56.25%' }}>
-            <div className='absolute top-0 left-0 right-0 bottom-0 max-w-full rounded-md h-auto w-full bg-base-200 animate-pulse' />
-          </div>
-          <VideoDetailInfoSkeleton />
-        </div>
-        <div className='basis-96 w-96 max-2xl:hidden my-4 shrink-0 mx-3' />
-      </div>
-    );
-  }
+  //if (isLoading) {
+  //  return (
+  //    <div className='w-full flex min-h-[85vh]'>
+  //      <div className='grow my-4'>
+  //        <div className='m-3/5 relative overflow-hidden' style={{ paddingTop: '56.25%' }}>
+  //          <div className='absolute top-0 left-0 right-0 bottom-0 max-w-full rounded-md h-auto w-full bg-base-200 animate-pulse' />
+  //        </div>
+  //        <VideoDetailInfoSkeleton />
+  //      </div>
+  //      <div className='basis-96 w-96 max-2xl:hidden my-4 shrink-0 mx-3' />
+  //    </div>
+  //  );
+  //}
 
   if (updateOpen) {
     return <UploadLayout />;
@@ -92,14 +89,14 @@ export default function VideoDetailLayout() {
       <div className='grow my-4'>
         <div className='m-3/5 relative overflow-hidden' style={{ paddingTop: '56.25%' }}>
           <div className='absolute top-0 left-0 right-0 bottom-0 max-w-full rounded-md h-auto w-full text-center'>
-            <VideoPlayer sources={data.videoUrl} styles={videoArgs.styles} videoOptions={videoArgs.videoOptions} />
+            <VideoPlayer sources={video.videoUrl} styles={videoArgs.styles} videoOptions={videoArgs.videoOptions} />
           </div>
         </div>
-        <VideoDetailInfo video={data} handleUpdatePage={handleUpdatePage} isMyVideo={isMyVideo} />
-        <Comments video={data} />
+        <VideoDetailInfo video={video} handleUpdatePage={handleUpdatePage} isMyVideo={isMyVideo} />
+        <Comments video={video} />
       </div>
       <div className='basis-96 w-96 max-2xl:hidden my-4 shrink-0 mx-3'>
-        {data.recommendedList?.map((v: VideoCardType) => <VideoSummaryItemRow key={v.videoId} videoSummaryItem={v} />)}
+        {video.recommendedList?.map((v: VideoCardType) => <VideoSummaryItemRow key={v.videoId} videoSummaryItem={v} />)}
       </div>
     </div>
   );
